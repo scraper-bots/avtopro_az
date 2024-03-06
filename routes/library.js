9b68873188c0eed5572ee9115b10639efd687833
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-06 12:54:52 +0400

    check

--- routes/library.js
+++ routes/library.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const libraryController = require('../controllers/libraryController');
 const { validateAddEntry } = require('../middlewares/validation');
 
+
 router.get('/', libraryController.getAllEntries);
 router.get('/add', libraryController.getAddEntryForm);
 router.post('/add', validateAddEntry, libraryController.addEntry);
